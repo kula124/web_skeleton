f04c82a8cfc955fa0e11f54386f887f616a56755
kula <kula124@gmail.com>
2020-06-17 18:02:29 +0200

    adding scss/css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,49 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.join(__dirname, 'src')
+    ]
+  },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.js?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"]
-        }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", {
+          loader: "css-loader", options: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: '[name]--[local]--[hash:base64:8]'
+          }
+        }]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          {
+            loader: "css-loader", options: {
+              modules: {
+                localIdentName: '[local]--[hash:base64:8]'
+              },
+              importLoaders: 1,
+            }
+          },
+          // Compiles Sass to CSS
+          'sass-loader'
+        ],
       }
     ]
   },
