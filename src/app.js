bc556e548896eafd21da32a105533e0321a8a36a
kula <kula124@gmail.com>
2020-06-17 18:02:54 +0200

    adding root app component

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles/global.scss'
+
+const app = () => (
+    <h1>
+        <p>Testing 123</p>
+        <p styleName='test'>Welcome to React!</p>
+    </h1>
+)
+
+export default app
