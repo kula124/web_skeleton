cc63a0cf00704248e6a310b858920f7c8af8fa32
kula <kula124@gmail.com>
2020-06-17 18:36:27 +0200

    linter added

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './styles/global.scss'
 
 const app = () => (
-    <h1>
-        <p>Testing 123</p>
-        <p styleName='test'>Welcome to React!</p>
-    </h1>
+  <h1>
+    <p>Testing 123</p>
+    <p styleName='test'>Welcome to React!</p>
+  </h1>
 )
 
 export default app
