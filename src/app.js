47aba4945a224cc30a6a613076893d841e144404
kula <kula124@gmail.com>
2020-12-30 16:13:29 +0100

    setting up document head and icon

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './styles/global.scss'
 import './styles/global.module.scss'
+import { Helmet } from 'react-helmet'
+
+import icon from '../favicon.png'
+import Favicon from 'react-favicon'
 
 const app = () => (
-  <h1>
-    <p styleName='test-module'>Testing 123</p>
-    <p className='test'>Welcome to React!</p>
-  </h1>
+  <>
+    <Helmet>
+      <title>WebSkeleton for React!</title>
+    </Helmet>
+    <Favicon url={icon} />
+    <h1>
+      <p styleName='test-module'>Testing 123</p>
+      <p className='test'>Welcome to React!</p>
+    </h1>
+  </>
 )
 
 export default app
